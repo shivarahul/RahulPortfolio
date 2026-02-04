d2fb7d75380e6ea347665b95e411ebfa744a39f8
rahulshivanathri <your-username@users.noreply.github.com>
2026-02-04 11:55:31 -0600

    updated changes

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -2,35 +2,35 @@ export const experienceData = [
     {
         id: 1,
         company: 'USAA',
-        jobtitle: 'Java Full Stack AWS Developer',
+        jobtitle: 'Senior Software Engineer',
         startYear: 'May 2024',
         endYear: 'Present'
     },
     {
         id: 2,
         company: 'Digital Ellanky',
-        jobtitle: 'Java Lead Engineer',
+        jobtitle: 'Software Engineer',
         startYear: 'Jan 2022',
         endYear: 'Aug 2022'
     },
     {
         id: 3,
         company: 'Jiyyo Innovations',
-        jobtitle: 'Java Full Stack Developer',
+        jobtitle: 'Software Engineer',
         startYear: 'Jul 2021',
         endYear: 'Jan 2022'
     },
     {
         id: 4,
         company: 'Cognizant Technology Solutions',
-        jobtitle: 'Java Full Stack Developer',
+        jobtitle: 'Software Engineer',
         startYear: 'Dec 2018',
         endYear: 'Jul 2021'
     },
     {
         id: 5,
         company: 'Publicis Sapient',
-        jobtitle: 'Java Full Stack Developer',
+        jobtitle: 'Software Developer Intern',
         startYear: 'Mar 2018',
         endYear: 'Jul 2018'
     },
